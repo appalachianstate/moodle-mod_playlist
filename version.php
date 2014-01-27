c944665f4a175647fafac8b403fcd9b6801f6c8e
Fred Woolard <woolardfa@appstate.edu>
2014-01-27 10:24:48 -0500

    Ver. 2013052001 Release 0.0.3 (Build 2014012300)

--- version.php
+++ version.php
@@ -31,9 +31,9 @@
 
     defined('MOODLE_INTERNAL') || die();
 
-    $module->version   = 2013052000;
+    $module->version   = 2013052001;
     $module->requires  = 2012062500;
     $module->cron      = 0;
     $module->component = 'mod_playlist';
     $module->maturity  = MATURITY_STABLE;
-    $module->release   = '0.0.2 (Build 2013100200)';
+    $module->release   = '0.0.3 (Build 2014012300)';
